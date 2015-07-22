3510ac0e0135b79cbc300b484ce2db1162cc5e77
dornathal <haase.fabian+git@gmail.com>
2015-07-22 16:47:27 +0200

    --

--- .generated/config.php
+++ .generated/config.php
@@ -1,12 +1,19 @@
 <?php
+
+define('DB_HOST',getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
 $serviceContainer = \Propel\Runtime\Propel::getServiceContainer();
 $serviceContainer->checkVersion('2.0.0-dev');
 $serviceContainer->setAdapterClass('default', 'mysql');
 $manager = new \Propel\Runtime\Connection\ConnectionManagerSingle();
 $manager->setConfiguration(array (
-  'dsn' => 'mysql:host='.$OPENSHIFT_MYSQL_DB_NAME.':'.$OPENSHIFT_MYSQL_DB_PORT.';dbname=ivta',
-  'user' => 'adminCvZi4P4',
-  'password' => ' iXwLl77Qk8KY',
+  'dsn' => 'mysql:host='.DB_HOST.':'.DB_PORT.';dbname='.DB_NAME,
+  'user' => DB_USER,
+  'password' => DB_PASS,
   'settings' =>
   array (
     'charset' => 'utf8',
