00f933ee8d2058d4e0f9283e031452934bd26048
dornathal <haase.fabian+git@gmail.com>
2015-07-28 15:30:58 +0200

    added pagination

--- website/Model/AirportQuery.php
+++ website/Model/AirportQuery.php
@@ -6,16 +6,7 @@ use Model\Base\AirportQuery as BaseAirportQuery;
 use Model\Map\AirportTableMap;
 use Propel\Runtime\ActiveQuery\Criteria;
 
-/**
- * Skeleton subclass for performing query and update operations on the 'airports' table.
- *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
- */
+
 class AirportQuery extends BaseAirportQuery
 {
     /**
